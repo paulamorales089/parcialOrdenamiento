d96a0b759e77f19a9e1520e49fb7cd600a1c473a
paulamorales089 <paitomorales03@gmail.com>
2021-04-26 00:15:10 -0500

    comparator done

--- src/model/DogBreed.java
+++ src/model/DogBreed.java
@@ -1,5 +1,12 @@
 package model;
 
-public class DogBreed {
+import java.util.Comparator;
+
+public class DogBreed implements Comparator <Dogs>{
+
+	@Override
+	public int compare(Dogs o1, Dogs o2) {
+	 return o1.getBreed().compareTo(o2.getBreed());
+	}
 
 }
